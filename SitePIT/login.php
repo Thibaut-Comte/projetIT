e8a508aefe858d15066b42128b347d4b2bb1b2ca
Thibaut-Comte <thibaut.comte.1@gmail.com>
2018-02-02 11:31:52 +0100

    login / register / forgot

--- SitePIT/login.php
+++ SitePIT/login.php
@@ -4,6 +4,13 @@
 <head>
     <?php require 'includes/head.html'; ?>
     <link rel="stylesheet" href="./style/border.css" media="screen,projection" type="text/css">
+    <!-- All the files that are required -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/style/login.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
     <title>SCPI : Bourse d'échange</title>
 </head>
 <body class="container-fluid">
@@ -11,18 +18,69 @@
     <?php require 'includes/header.html'; ?>
 </header>
 <main>
-    <div class="container">
-        <div class="row">
-            <?php ?>
-            <div class="col-xs-offset-2 col-xs-10">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <img src="./images/bien.jpg" class="img-responsive center-block">
+    <!-- Where all the magic happens -->
+    <!-- LOGIN FORM -->
+    <div class="text-center" style="padding:50px 0">
+        <div class="logo">Login</div>
+        <!-- Main Form -->
+        <div class="login-form-1">
+            <form id="login-form" class="text-left">
+                <div class="login-form-main-message"></div>
+                <div class="main-login-form">
+                    <div class="login-group">
+                        <div class="form-group">
+                            <label for="lg_username" class="sr-only">Username</label>
+                            <input type="text" class="form-control" id="lg_username" name="lg_username" placeholder="username">
+                        </div>
+                        <div class="form-group">
+                            <label for="lg_password" class="sr-only">Password</label>
+                            <input type="password" class="form-control" id="lg_password" name="lg_password" placeholder="password">
+                        </div>
+                        <div class="form-group login-group-checkbox">
+                            <input type="checkbox" id="lg_remember" name="lg_remember">
+                            <label for="lg_remember">remember</label>
+                        </div>
                     </div>
+                    <button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>
                 </div>
-            </div>
+                <div class="etc-login-form">
+                    <p>forgot your password? <a href="forgot.html">click here</a></p>
+                    <p>new user? <a href="register.html">create new account</a></p>
+                </div>
+            </form>
         </div>
+        <!-- end:Main Form -->
     </div>
+
+<!--    -->
+<!---->
+   <!-- FORGOT PASSWORD FORM -->
+<!--    <div class="text-center" style="padding:50px 0">-->
+<!--        <div class="logo">forgot password</div>-->
+<!--         Main Form -->
+<!--        <div class="login-form-1">-->
+<!--            <form id="forgot-password-form" class="text-left">-->
+<!--                <div class="etc-login-form">-->
+<!--                    <p>When you fill in your registered email address, you will be sent instructions on how to reset your password.</p>-->
+<!--                </div>-->
+<!--                <div class="login-form-main-message"></div>-->
+<!--                <div class="main-login-form">-->
+<!--                    <div class="login-group">-->
+<!--                        <div class="form-group">-->
+<!--                            <label for="fp_email" class="sr-only">Email address</label>-->
+<!--                            <input type="text" class="form-control" id="fp_email" name="fp_email" placeholder="email address">-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>-->
+<!--                </div>-->
+<!--                <div class="etc-login-form">-->
+<!--                    <p>already have an account? <a href="#">login here</a></p>-->
+<!--                    <p>new user? <a href="#">create new account</a></p>-->
+<!--                </div>-->
+<!--            </form>-->
+<!--        </div>-->
+<!--         end:Main Form
+ <!--   </div> -->
 </main>
 <aside>
 
@@ -32,3 +90,6 @@
 </footer>
 </body>
 </html>
+
+
+
